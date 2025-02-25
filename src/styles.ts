4f8ebfc86486e69de36dc2229c5a8210b2a839be
Willian <willianvia1@gmail.com>
2025-02-24 23:05:05 -0300

    criando a seção de projetos e o componente de projetos

--- src/styles.ts
+++ src/styles.ts
@@ -2,9 +2,10 @@ import styled, { createGlobalStyle } from 'styled-components'
 
 const EstiloGlobal = createGlobalStyle`
 * {
-margin:0;
-padding:0;
-font-family: "Inter", serif;
+  margin:0;
+  padding:0;
+  font-family: "Inter", serif;
+  list-style: none;
 }
 
 body {
