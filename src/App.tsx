9ddfc94d1d7a06dd17c49e617d7def370f751b14
Willian <willianvia1@gmail.com>
2025-03-03 23:45:25 -0300

    aplicando tema light e dark e alterando a responsividade

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,31 @@
+import { ThemeProvider } from 'styled-components'
+import { useState } from 'react'
+
 import Projects from './containers/Projects'
 import Sidebar from './containers/Sidebar'
 import About from './containers/Sobre'
 import EstiloGlobal, { Container } from './styles'
+import temaLight from './themes/light'
+import temaDark from './themes/dark'
 
 function App() {
+  const [estaUsandoTemaDark, setEstaUsandoTemaDark] = useState(false)
+
+  function trocaTema() {
+    setEstaUsandoTemaDark(!estaUsandoTemaDark)
+  }
+
   return (
-    <>
+    <ThemeProvider theme={estaUsandoTemaDark ? temaDark : temaLight}>
       <EstiloGlobal />
       <Container>
-        <Sidebar />
+        <Sidebar trocaTema={trocaTema} />
         <main>
           <About />
           <Projects />
         </main>
       </Container>
-    </>
+    </ThemeProvider>
   )
 }
 
